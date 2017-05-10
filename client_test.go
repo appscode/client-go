20d7326eb64ea8e8a0296cd86b2085b672229ab9
tamal <tamal@appscode.com>
2017-05-10 08:33:40 -0700

    Use grpc status

--- client_test.go
+++ client_test.go
@@ -17,33 +17,23 @@ func TestKubernetesClient(t *testing.T) {
 		Cluster: "qa",
 		Type:    "pods",
 	}
-	respV1beta2Client, err := c.Kubernetes().V1beta2().Client().List(c.Context(), reqV1beta2)
+	_, err := c.Kubernetes().V1beta2().Client().List(c.Context(), reqV1beta2)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Status Kubernetes V1beta2 Client: ", respV1beta2Client.Status)
-	reqV1beta1Client := &kv1beta1.ClientRequest{}
-	respV1beta1, err := c.Kubernetes().V1beta1().Client().Pods(c.Context(), reqV1beta1Client)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Status Kubernetes V1beta1 Client:", respV1beta1.Status)
 	//LoadBalancer client test
 	req := &kv1beta1.ListRequest{
 		Cluster: "qa",
 	}
-	resp, err := c.Kubernetes().V1beta1().LoadBalancer().List(c.Context(), req)
-	fmt.Println("Status Loadbalancer v1beta1: ", resp.Status)
+	_, err = c.Kubernetes().V1beta1().LoadBalancer().List(c.Context(), req)
 	//Disk client Test
 	reqV1beta2Disk := &kv1beta2.DiskListRequest{
 		Cluster: "qa",
 	}
-	respV1beta2Disk, err := c.Kubernetes().V1beta2().Disk().List(c.Context(), reqV1beta2Disk)
+	_, err = c.Kubernetes().V1beta2().Disk().List(c.Context(), reqV1beta2Disk)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Status Kubernetes V1beta2 Disk:", respV1beta2Disk.Status)
-
 }
 
 func TestAuthClient(t *testing.T) {
@@ -56,7 +46,6 @@ func TestAuthClient(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Status Auth project:", resp.Status)
 }
 
 func getClient() *Client {
