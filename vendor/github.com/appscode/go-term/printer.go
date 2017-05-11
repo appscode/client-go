0d74954b633628967f83891d55a839db440c5d9e
Tamal Saha <tamal@appscode.com>
2017-05-11 10:05:04 -0700

    Use grpc status (#8)

--- vendor/github.com/appscode/go-term/printer.go
+++ vendor/github.com/appscode/go-term/printer.go
@@ -36,14 +36,14 @@ func Successln(i ...interface{}) {
 func Errorln(i ...interface{}) {
 	doPrintln([]color.Attribute{color.FgRed, color.Bold}, i)
 	if Env != env.Prod {
-		errors.Log(errors.New().Internal())
+		fmt.Fprintln(os.Stderr, errors.New().Error())
 	}
 }
 
 func Fatalln(i ...interface{}) {
 	doPrintln([]color.Attribute{color.FgRed, color.Bold}, i)
 	if Env != env.Prod {
-		errors.Log(errors.New().Internal())
+		fmt.Fprintln(os.Stderr, errors.New().Error())
 	}
 	os.Exit(1)
 }
@@ -52,7 +52,7 @@ func ExitOnError(err error) {
 	if err != nil {
 		doPrintln([]color.Attribute{color.FgRed, color.Bold}, []interface{}{err.Error()})
 		if Env != env.Prod {
-			errors.Log(err)
+			fmt.Fprintln(os.Stderr, errors.New().Error())
 		}
 		os.Exit(1)
 	}
