8a69c2ddebc5a5035b6c6320444ded4db27b3dae
tamal <tamal@appscode.com>
2017-11-07 03:11:53 -0800

    Revise cluster api with Pharmer objects

--- multi_service.go
+++ multi_service.go
@@ -5,7 +5,8 @@ import (
 	auth "github.com/appscode/api/auth/v1beta1"
 	ca "github.com/appscode/api/certificate/v1beta1"
 	ci "github.com/appscode/api/ci/v1beta1"
-	kubernetesV1beta1 "github.com/appscode/api/kubernetes/v1beta1"
+	cluster_v1alpha1 "github.com/appscode/api/cluster/v1alpha1"
+	k8s_v1beta1 "github.com/appscode/api/kubernetes/v1beta1"
 	namespace "github.com/appscode/api/namespace/v1beta1"
 	"google.golang.org/grpc"
 )
@@ -18,6 +19,7 @@ type multiClientInterface interface {
 	CA() *caService
 	CI() *ciService
 	Namespace() *nsService
+	Cluster() *versionedClusterService
 	Kubernetes() *versionedKubernetesService
 }
 
@@ -27,6 +29,7 @@ type multiClientServices struct {
 	caClient                  *caService
 	ciClient                  *ciService
 	nsClient                  *nsService
+	versionedClusterClient    *versionedClusterService
 	versionedKubernetesClient *versionedKubernetesService
 }
 
@@ -48,13 +51,20 @@ func newMultiClientService(conn *grpc.ClientConn) multiClientInterface {
 			agentsClient:   ci.NewAgentsClient(conn),
 			metadataClient: ci.NewMetadataClient(conn),
 		},
+		versionedClusterClient: &versionedClusterService{
+			v1alpha1Service: &clusterV1alpha1Service{
+				credentialClient: cluster_v1alpha1.NewCredentialsClient(conn),
+				clusterClient:    cluster_v1alpha1.NewClustersClient(conn),
+				nodeGroupClient:  cluster_v1alpha1.NewNodeGroupsClient(conn),
+				sshConfigClient:  cluster_v1alpha1.NewSSHConfigClient(conn),
+				metdataClient:    cluster_v1alpha1.NewMetadataClient(conn),
+			},
+		},
 		versionedKubernetesClient: &versionedKubernetesService{
 			v1beta1Service: &kubernetesV1beta1Service{
-				clusterClient:  kubernetesV1beta1.NewClustersClient(conn),
-				incidentClient: kubernetesV1beta1.NewIncidentsClient(conn),
-				metdataClient:  kubernetesV1beta1.NewMetadataClient(conn),
-				clientsClient:  kubernetesV1beta1.NewClientsClient(conn),
-				diskClient:     kubernetesV1beta1.NewDisksClient(conn),
+				incidentClient: k8s_v1beta1.NewIncidentsClient(conn),
+				clientsClient:  k8s_v1beta1.NewClientsClient(conn),
+				diskClient:     k8s_v1beta1.NewDisksClient(conn),
 			},
 		},
 		nsClient: &nsService{
@@ -81,6 +91,10 @@ func (s *multiClientServices) CA() *caService {
 
 func (s *multiClientServices) CI() *ciService {
 	return s.ciClient
+}
+
+func (s *multiClientServices) Cluster() *versionedClusterService {
+	return s.versionedClusterClient
 }
 
 func (s *multiClientServices) Kubernetes() *versionedKubernetesService {
@@ -149,6 +163,42 @@ func (c *caService) CertificatesClient() ca.CertificatesClient {
 	return c.certificateClient
 }
 
+type versionedClusterService struct {
+	v1alpha1Service *clusterV1alpha1Service
+}
+
+func (v *versionedClusterService) V1alpha1() *clusterV1alpha1Service {
+	return v.v1alpha1Service
+}
+
+type clusterV1alpha1Service struct {
+	credentialClient cluster_v1alpha1.CredentialsClient
+	clusterClient    cluster_v1alpha1.ClustersClient
+	nodeGroupClient  cluster_v1alpha1.NodeGroupsClient
+	sshConfigClient  cluster_v1alpha1.SSHConfigClient
+	metdataClient    cluster_v1alpha1.MetadataClient
+}
+
+func (k *clusterV1alpha1Service) Credential() cluster_v1alpha1.CredentialsClient {
+	return k.credentialClient
+}
+
+func (k *clusterV1alpha1Service) Cluster() cluster_v1alpha1.ClustersClient {
+	return k.clusterClient
+}
+
+func (k *clusterV1alpha1Service) NodeGroup() cluster_v1alpha1.NodeGroupsClient {
+	return k.nodeGroupClient
+}
+
+func (k *clusterV1alpha1Service) SSHConfig() cluster_v1alpha1.SSHConfigClient {
+	return k.sshConfigClient
+}
+
+func (k *clusterV1alpha1Service) Metadata() cluster_v1alpha1.MetadataClient {
+	return k.metdataClient
+}
+
 type versionedKubernetesService struct {
 	v1beta1Service *kubernetesV1beta1Service
 }
@@ -158,29 +208,19 @@ func (v *versionedKubernetesService) V1beta1() *kubernetesV1beta1Service {
 }
 
 type kubernetesV1beta1Service struct {
-	clusterClient  kubernetesV1beta1.ClustersClient
-	incidentClient kubernetesV1beta1.IncidentsClient
-	metdataClient  kubernetesV1beta1.MetadataClient
-	clientsClient  kubernetesV1beta1.ClientsClient
-	diskClient     kubernetesV1beta1.DisksClient
+	incidentClient k8s_v1beta1.IncidentsClient
+	clientsClient  k8s_v1beta1.ClientsClient
+	diskClient     k8s_v1beta1.DisksClient
 }
 
-func (k *kubernetesV1beta1Service) Cluster() kubernetesV1beta1.ClustersClient {
-	return k.clusterClient
-}
-
-func (a *kubernetesV1beta1Service) Incident() kubernetesV1beta1.IncidentsClient {
+func (a *kubernetesV1beta1Service) Incident() k8s_v1beta1.IncidentsClient {
 	return a.incidentClient
 }
 
-func (k *kubernetesV1beta1Service) Metadata() kubernetesV1beta1.MetadataClient {
-	return k.metdataClient
-}
-
-func (k *kubernetesV1beta1Service) Client() kubernetesV1beta1.ClientsClient {
+func (k *kubernetesV1beta1Service) Client() k8s_v1beta1.ClientsClient {
 	return k.clientsClient
 }
 
-func (k *kubernetesV1beta1Service) Disk() kubernetesV1beta1.DisksClient {
+func (k *kubernetesV1beta1Service) Disk() k8s_v1beta1.DisksClient {
 	return k.diskClient
 }
